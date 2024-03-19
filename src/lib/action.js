823a99f5b47a2fc5ea7956597a92181fbacf6649
Yesda0 <ssu22ndy@soongsil.ac.kr>
2024-03-20 03:57:38 +0900

    feat/Admin 구현

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,8 +1,52 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { signIn, signOut } from "./auth";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+
+export const addUser = async (prevState, formData) => {
+  const { name, email, password, isAdmin } = Object.fromEntries(formData);
+
+  try {
+    const prisma = new PrismaClient({});
+    const isAdminBool = isAdmin === "true";
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        isAdmin: isAdminBool,
+      },
+    });
+
+    console.log("saved to db");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const deleteUser = async (formData) => {
+  const { email } = Object.fromEntries(formData);
+
+  try {
+    const prisma = new PrismaClient({});
+
+    await prisma.user.delete({
+      where: {
+        email: email,
+      },
+    });
+
+    console.log("deleted from db");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
 
 export const handleLogout = async () => {
   "use server";
