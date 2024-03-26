646ebd683a049c1ba6ffc5f35ce001923ea99175
Yesda0 <ssu22ndy@soongsil.ac.kr>
2024-03-27 03:27:08 +0900

    fix/SessionMaxAge 세션 만료시간 설정_1시간

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+//import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 import { handleLogout } from "@/lib/action";
